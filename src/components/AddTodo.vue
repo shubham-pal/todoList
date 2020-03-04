1592b74bb802be9a60e39df396136c442464bbe3
shubham-pal <shubham.mca19.du@gmail.com>
2020-03-05 00:02:47 +0530

    add to todolist

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <form>
+        <form @submit="addTodo">
             <input type="text" v-model="title" name="title" placeholder="Add Todo...">
             <input type="submit" value="Submit" class="btn">
         </form>
@@ -8,12 +8,28 @@
 </template>
 
 <script>
+import uuid from 'uuid';
+
 export default {
   name: 'AddTodo',
   data() {
     return {
       title: '',
     };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodo = {
+        id: uuid.v4(),
+        title: this.title,
+        completed: false,
+      };
+      // send up to parent
+      this.$emit('add-todo', 'newTodo');
+
+      this.title = '';
+    },
   },
 };
 </script>
