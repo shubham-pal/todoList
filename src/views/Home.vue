a18bac452b37499d5c921130f59c342bf901ce94
shubham-pal <shubham.mca19.du@gmail.com>
2020-03-05 20:58:50 +0530

    vue-router add and changes in main app.vue" s

---
+++ src/views/Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Header from '../components/layout/Header.vue';
+import Todos from '../components/Todos.vue';
+import AddTodo from '../components/AddTodo.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Todos,
+    Header,
+    AddTodo,
+  },
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    },
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then((res) => { this.todos = res.data; })
+      .catch((err) => console.log(err));
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn-hover {
+  background: #666;
+}
+</style>
